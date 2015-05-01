1e8f9765716b552c64efa5a756f3056e83a1a89b
maximustag <maximustag@gmail.com>
2015-05-01 11:07:03 +0300

    Added interface ISpaRepositoty

---
+++ Spa/Infrastructure/ISpaRepository.cs
@@ -0,0 +1,29 @@
+﻿using Spa.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Spa.Infrastructure
+{
+    public class ISpaRepository
+    {
+        IQueryable<Product> GetAllProducts();
+        IQueryable<Product> GetProductsByOrder(int orderId);
+        Product GetProduct(int productId);
+
+        IQueryable<Order> GetAllOrders();
+        IQueryable<Order> GetOrdersByCustomer(int customerId);
+        Order GetOrder(int orderId);
+
+        IQueryable<Customer> GetAllCustomers();
+        IQueryable<Customer> GetAllCustomersWithOrders();
+        Customer GetCustomerByOrder(int orderId);
+        Customer GetCustomer(int customerId);
+        
+
+
+
+
+    }
+}
--- Spa/Migrations/Configuration.cs
+++ Spa/Migrations/Configuration.cs
@@ -1,31 +1,24 @@
 namespace Spa.Migrations
 {
-    using System;
-    using System.Data.Entity;
-    using System.Data.Entity.Migrations;
-    using System.Linq;
+    using Spa.Infrastructure;
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<Spa.Infrastructure.ApplicationDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
         }
 
-        protected override void Seed(Spa.Infrastructure.ApplicationDbContext context)
+#if DEBUG
+        protected override void Seed(ApplicationDbContext)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            
         }
+#endif
     }
 }
